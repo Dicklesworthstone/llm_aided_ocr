a43f0c0b8814c8584f35da09f01a7342ff556a2e
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-08-01 18:50:04 -0400

    Update tesseract_with_llama2_corrections.py

--- tesseract_with_llama2_corrections.py
+++ tesseract_with_llama2_corrections.py
@@ -1,23 +1,15 @@
 from pdf2image import convert_from_path
 import pytesseract
 from llama_cpp import Llama
-
+from multiprocessing import Pool
+from tqdm import tqdm
+import os
 
 # sudo apt-get install -y tesseract-ocr libtesseract-dev poppler-utils
 # git lfs install
 # git clone https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML
-# python3 -m venv venv
-# source venv/bin/activate
-# python3 -m pip install --upgrade pip
-# python3 -m pip install wheel
-# pip install -r requirements.txt
 
-llm = Llama(model_path="./Llama-2-13B-chat-GGML/llama-2-13b-chat.ggmlv3.q4_0.bin", n_ctx=2048)
 
-print(f"Tesseract version: {pytesseract.get_tesseract_version()}")
-
-input_pdf_file_path = 'royalnavyhistor03clow.pdf'
-max_test_pages = 30
 
 def convert_pdf_to_images_func(input_pdf_file_path, max_test_pages):
     print(f"Now converting first {max_test_pages} pages of PDF file {input_pdf_file_path} to images...")
@@ -35,31 +27,84 @@ def check_extracted_pages_func(extracted_text_string):
         return False
     return extracted_text_string
 
-def process_text_with_llm_func(extracted_text_string):
-    # prompt_text_1 = f"Q: Is this valid English text? (y/n): ```{extracted_text_string}``` A: _|_"
+def remove_intro(llm_output_2_text):
+    # Strip leading and trailing whitespace before splitting the lines
+    lines = llm_output_2_text.strip().splitlines()
+    # Skip the first line and the following blank line
+    lines = lines[2:] if lines[1].strip() == '' else lines[1:]
+    return '\n'.join(lines)
+
+def is_valid_english(llm_output_1_text):
+    # Use the lower() function to make the string comparison case-insensitive.
+    # Use the strip() function to remove any leading or trailing white space.
+    # Then check if the string starts with 'yes'.
+    return llm_output_1_text.lower().strip().startswith('yes')
+
+def process_text_with_llm_func(extracted_text_string, check_if_valid_english=False, reformat_as_markdown=True):
     max_tokens = 2*len(extracted_text_string) + 50
+    prompt_text_1 = f"Q: Is this valid English text? (y/n): ```{extracted_text_string}``` A: _|_"
     prompt_text_2 = f"Q: Correct any typos caused by bad OCR in this text, using common sense reasoning, responding only with the corrected text: ```{extracted_text_string}``` A: _|_"
-    # llm_output_1 = llm(prompt_text_1, max_tokens=max_tokens, stop=["Q:", "_|_"], echo=True)
-    # llm_output_1_text = llm_output_1["choices"][0]["text"].replace(prompt_text_1, "")
-    # print(f"llm_output_1_text: {llm_output_1_text}")
-    llm_output_1_text = ''
-    llm_output_2 = llm(prompt_text_2, max_tokens=max_tokens, stop=["Q:", "_|_"], echo=True)
-    llm_output_2_text = llm_output_2["choices"][0]["text"].replace(prompt_text_2, "")
-    print(f"llm_output_2_text: {llm_output_2_text}")    
-    return llm_output_1_text, llm_output_2_text
+    if check_if_valid_english:
+        llm_output_1 = llm(prompt_text_1, max_tokens=max_tokens, stop=["Q:", "_|_"], echo=True)
+        llm_output_1_text = llm_output_1["choices"][0]["text"].replace(prompt_text_1, "")
+        valid_english = is_valid_english(llm_output_1_text)
+    else:
+        valid_english = False
+    if valid_english or not check_if_valid_english:
+        llm_output_2 = llm(prompt_text_2, max_tokens=max_tokens, stop=["Q:", "_|_"], echo=True)
+        llm_output_2_text = llm_output_2["choices"][0]["text"].replace(prompt_text_2, "")
+        corrected_extracted_text_string = remove_intro(llm_output_2_text)
+    if reformat_as_markdown:
+        prompt_test_3 = f"Q: Reformat this text to be more readable using markdown formatting and using common sense reasoning; respond only with the reformatted text: ```{corrected_extracted_text_string}``` A: _|_"    
+        llm_output_3 = llm(corrected_extracted_text_string, max_tokens=max_tokens, stop=["Q:", "_|_"], echo=True)
+        corrected_extracted_text_string = llm_output_3["choices"][0]["text"].replace(prompt_test_3, "")
+    return corrected_extracted_text_string
 
-list_of_extracted_text_strings = []
-list_of_scanned_images = convert_pdf_to_images_func(input_pdf_file_path, max_test_pages)
+def ocr_image(image):
+    return pytesseract.image_to_string(image)
+    
 
-skip_first_n_pages = 20
-print(f"Extracting text from converted pages...")
-for ii, current_image in enumerate(list_of_scanned_images):
-    if ii < skip_first_n_pages:
-        continue
-    text = pytesseract.image_to_string(current_image)
-    extracted_text_string = check_extracted_pages_func(text)
-    if extracted_text_string:
-        print(f"\nText from page {ii + 1}:")
-        llm_output_1_text, llm_output_2_text = process_text_with_llm_func(extracted_text_string)
-        list_of_extracted_text_strings.append(llm_output_2_text)
-        print(text)
+if __name__ == '__main__':
+    input_pdf_file_path = '160301289-Warren-Buffett-Katharine-Graham-Letter.pdf'
+    max_test_pages = 3
+    skip_first_n_pages = 0
+    reformat_as_markdown = False
+    check_if_valid_english = False
+    list_of_scanned_images = convert_pdf_to_images_func(input_pdf_file_path, max_test_pages)
+
+    model_file_path = "./Llama-2-13B-chat-GGML/llama-2-13b-chat.ggmlv3.q4_0.bin"
+    print(f"Loading Llama model from {model_file_path}...")
+    llm = Llama(model_path=model_file_path, n_ctx=2048)
+    print(f"Tesseract version: {pytesseract.get_tesseract_version()}")
+
+    print(f"Extracting text from converted pages...")
+    with Pool() as p:
+        list_of_extracted_text_strings = list(tqdm(p.map(ocr_image, list_of_scanned_images), total=len(list_of_scanned_images)))
+
+    # process the OCR output
+    list_of_corrected_text_strings = []
+    for ii, text in enumerate(list_of_extracted_text_strings):
+        if ii < skip_first_n_pages:
+            continue
+        extracted_text_string = check_extracted_pages_func(text)
+        if extracted_text_string:
+            print(f"\nText from page {ii + 1}:")
+            corrected_extracted_text_string = process_text_with_llm_func(extracted_text_string, check_if_valid_english, reformat_as_markdown)
+            list_of_corrected_text_strings.append(corrected_extracted_text_string)
+            print(text)
+
+    # join the list of strings into a single string with a newline after each page
+    final_text = "\n".join(list_of_corrected_text_strings)
+
+    # get the base name of the input file (without the extension)
+    base_name = os.path.splitext(input_pdf_file_path)[0]
+    # choose the extension based on the reformat_as_markdown flag
+    output_extension = '.md' if reformat_as_markdown else '.txt'
+    # create the output file path
+    output_file_path = base_name + output_extension
+
+    # write the final text to the output file
+    with open(output_file_path, 'w') as f:
+        f.write(final_text)
+
+    print(f"Final text written to: {output_file_path}")
