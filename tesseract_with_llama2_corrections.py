4392c4a588a221c63148aa4b661fa769d8b15eaf
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-08-02 11:28:53 -0400

    Update tesseract_with_llama2_corrections.py

--- tesseract_with_llama2_corrections.py
+++ tesseract_with_llama2_corrections.py
@@ -2,11 +2,13 @@ from pdf2image import convert_from_path
 import pytesseract
 from llama_cpp import Llama
 from multiprocessing import Pool
-import os
 from langchain.embeddings import LlamaCppEmbeddings
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
+import os
 import pickle
+import hashlib
+import sqlite3
 
 # sudo apt-get install -y tesseract-ocr libtesseract-dev poppler-utils
 # git lfs install
@@ -27,10 +29,10 @@ def ocr_image(image):
     
 def check_extracted_pages_func(extracted_text_string):
     #first check if it's long enough to be a page:
-    if len(extracted_text_string) < 30:
+    if len(extracted_text_string) < 20:
         return False
     #now check if it has enough words to be a page:
-    if len(extracted_text_string.split()) < 20:
+    if len(extracted_text_string.split()) < 10:
         return False
     return extracted_text_string
 
@@ -72,73 +74,122 @@ def process_text_with_llm_func(extracted_text_string, check_if_valid_english=Fal
     return corrected_extracted_text_string
 
 def calculate_sentence_embedding(llama, text):
-    sentence_embedding = llama.embed_query(text)
+    sentence_embedding = None
+    while sentence_embedding is None:
+        try:
+            sentence_embedding = llama.embed_query(text)
+        except Exception as e:
+            if str(e) == "llama_tokenize_with_model: too many tokens":
+                text = text[:int(len(text)*0.95)]
+                print(f"Trimming sentence due to too many tokens. New length: {len(text)}")
+            else:
+                raise e
     return sentence_embedding
 
 def calculate_similarity(args):
     corrected_embedding, original_embedding = args
     return cosine_similarity([corrected_embedding], [original_embedding])
 
-def filter_hallucinations(corrected_text, raw_text, threshold=0.8, original_embeddings=None, corrected_embeddings=None):
+def filter_hallucinations(corrected_text, raw_text, threshold=0.1, pdf_file_path=None, db_path=None):
+    threshold_increment = 0.01
+    if db_path is not None and pdf_file_path is not None:
+        # Check if the database file exists
+        if not os.path.isfile(db_path):
+            print(f"No existing database found at {db_path}. Creating a new one.")
+        # Compute hash of the pdf file
+        sha3_256 = hashlib.sha3_256()
+        with open(pdf_file_path, "rb") as f:
+            for byte_block in iter(lambda: f.read(4096), b""):
+                sha3_256.update(byte_block)
+        file_hash = sha3_256.hexdigest()
+        # Connect to the database
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        # Create table if not exists
+        c.execute('''CREATE TABLE IF NOT EXISTS embeddings
+                     (file_hash text PRIMARY KEY, original_embeddings blob, corrected_embeddings blob)''')
+        # Try to fetch embeddings from the database
+        c.execute("SELECT * FROM embeddings WHERE file_hash=?", (file_hash,))
+        row = c.fetchone()
+        if row is not None:
+            original_embeddings = pickle.loads(row[1])
+            corrected_embeddings = pickle.loads(row[2])
+        else:
+            original_embeddings = None
+            corrected_embeddings = None
+    else:
+        original_embeddings = None
+        corrected_embeddings = None
     llama = LlamaCppEmbeddings(model_path=model_file_path)
     original_sentences = [s for s in raw_text.split('. ') if len(s) > 10]
     if original_embeddings is None:
         print("Calculating embeddings for original sentences...")
         original_embeddings = {s: calculate_sentence_embedding(llama, s) for s in tqdm(original_sentences)}
-        with open('original_embeddings.pkl', 'wb') as f:
-            pickle.dump(original_embeddings, f)
     corrected_sentences = [s for s in corrected_text.split('. ') if len(s) > 10]
     if corrected_embeddings is None:
         print("Calculating embeddings for corrected sentences...")
         corrected_embeddings = {s: calculate_sentence_embedding(llama, s) for s in tqdm(corrected_sentences)}
-        with open('corrected_embeddings.pkl', 'wb') as f:
-            pickle.dump(corrected_embeddings, f)        
-    filtered_sentences = []
-    print("Filtering sentences...")
-    for corrected_sentence in tqdm(corrected_sentences):
-        corrected_embedding = corrected_embeddings.get(corrected_sentence)
-        if corrected_embedding is None:  # Check for None embeddings
-            print(f"Could not get embedding for sentence: {corrected_sentence}")
-            continue  # skip this sentence or handle this case differently
-        # Prepare arguments for multiprocessing
-        args = [(corrected_embedding, original_embedding) for original_sentence, original_embedding in original_embeddings.items()]
-        # Create a multiprocessing pool and calculate similarities
-        with Pool() as pool:
-            similarities = pool.map(calculate_similarity, args)        
-        similarities = [s[0][0] for s in similarities]
-        max_similarity = max(similarities) if similarities else 0
-        if max_similarity >= threshold:
-            filtered_sentences.append(corrected_sentence)
-    return '. '.join(filtered_sentences), original_embeddings, corrected_embeddings
+    # Save the embeddings to the database
+    if db_path is not None and pdf_file_path is not None:
+        c.execute("INSERT OR REPLACE INTO embeddings VALUES (?, ?, ?)",
+                  (file_hash, pickle.dumps(original_embeddings), pickle.dumps(corrected_embeddings)))
+        conn.commit()
+    original_length = len(raw_text)
+    filtered_corrected_text = corrected_text
+    last_threshold = threshold
+    last_filtered_text = filtered_corrected_text
+    while True:
+        filtered_sentences = []
+        print(f"Filtering sentences with threshold {threshold}...")
+        for corrected_sentence in tqdm(corrected_sentences):
+            corrected_embedding = corrected_embeddings.get(corrected_sentence)
+            if corrected_embedding is None:  # Check for None embeddings
+                print(f"Could not get embedding for sentence: {corrected_sentence}")
+                continue  # skip this sentence or handle this case differently
+            similarities = [cosine_similarity([corrected_embedding], [original_embedding]) for original_sentence, original_embedding in original_embeddings.items()]
+            max_similarity = max(similarities) if similarities else 0
+            if max_similarity >= threshold:
+                filtered_sentences.append(corrected_sentence)
+        filtered_corrected_text = ". ".join(filtered_sentences)
+        print(f"Filtered text length: {len(filtered_corrected_text)}; original text length: {original_length}; difference (est. hallucinated text length): {len(filtered_corrected_text) - original_length}; Current threshold: {threshold}")        
+        if len(filtered_corrected_text) < original_length - 30:
+            filtered_corrected_text = last_filtered_text  # Use the filtered text from the previous iteration
+            threshold = last_threshold  # Use the threshold from the previous iteration
+            break
+        else:
+            last_threshold = threshold  # Store the current threshold before incrementing it
+            last_filtered_text = filtered_corrected_text  # Store the current filtered text before moving on to the next iteration
+            threshold += threshold_increment  # Increment the threshold if the condition is not met
+    print(f"Selected threshold: {threshold}")
+    return filtered_corrected_text, original_embeddings, corrected_embeddings
 
 
 if __name__ == '__main__':
-    input_pdf_file_path = '160301289-Warren-Buffett-Katharine-Graham-Letter.pdf'
+    #input_pdf_file_path = '160301289-Warren-Buffett-Katharine-Graham-Letter.pdf'
+    input_pdf_file_path = '18_PDFsam_How Atomic Submarines are Made (David C. Cooke).pdf'
     max_test_pages = 0 # set to 0 to convert all pages of the PDF file using Tesseract
     skip_first_n_pages = 0 # set to 0 to process all pages with the LLM
-    hallucination_similarity_threshold = 0.6 # The higher you set this, the more potential hallucinations will be filtered out (but also the more potential correct sentences will be filtered out)
+    starting_hallucination_similarity_threshold = 0.25 # The higher you set this, the more potential hallucinations will be filtered out (but also the more potential correct sentences will be filtered out)
     check_if_valid_english = False # set to True to check if the extracted text is valid English
     reformat_as_markdown = True # set to True to reformat the corrected extracted text using markdown formatting
     model_file_path = "./Llama-2-13B-chat-GGML/llama-2-13b-chat.ggmlv3.q4_0.bin"
-    test_filtering_hallucinations = False # set to True to test filtering hallucinations
+    sentence_embeddings_db_path = "./sentence_embeddings.sqlite"
+    test_filtering_hallucinations = True # set to True to test filtering hallucinations (for debugging purposes)
 
     if not test_filtering_hallucinations:
         list_of_scanned_images = convert_pdf_to_images_func(input_pdf_file_path, max_test_pages)
         print(f"Loading Llama model from {model_file_path}...")
         llm = Llama(model_path=model_file_path, n_ctx=2048)
         print(f"Tesseract version: {pytesseract.get_tesseract_version()}")
-
         print("Extracting text from converted pages...")
         with Pool() as p:
             list_of_extracted_text_strings = p.map(ocr_image, list_of_scanned_images)
         print("Done extracting text from converted pages. \n")
-
         raw_ocr_output = "\n".join(list_of_extracted_text_strings)
         base_name = os.path.splitext(input_pdf_file_path)[0]
         raw_ocr_output_file_path = f"{base_name}__raw_ocr_output.txt"
         with open(raw_ocr_output_file_path, "w") as f:
             f.write(raw_ocr_output)
-
         # process the OCR output
         list_of_corrected_text_strings = []
         for ii, text in enumerate(list_of_extracted_text_strings):
@@ -175,20 +226,14 @@ if __name__ == '__main__':
             final_text = f.read()
         with open(raw_ocr_output_file_path, 'r') as f:
             raw_ocr_output = f.read()
-        if 1: #Use saved embeddings
-            with open('original_embeddings.pkl', 'rb') as f:
-                original_embeddings = pickle.load(f)
-            with open('corrected_embeddings.pkl', 'rb') as f:
-                corrected_embeddings = pickle.load(f)
-            filtered_output, original_embeddings, corrected_embeddings = filter_hallucinations(final_text, raw_ocr_output, hallucination_similarity_threshold, original_embeddings, corrected_embeddings)
     
     print('Now filtering out hallucinations from corrected text...')
     # filter out hallucinations from the corrected output
-    filtered_output, original_embeddings, corrected_embeddings = filter_hallucinations(final_text, raw_ocr_output, hallucination_similarity_threshold)
+    filtered_output, original_embeddings, corrected_embeddings = filter_hallucinations(final_text, raw_ocr_output, starting_hallucination_similarity_threshold, input_pdf_file_path, sentence_embeddings_db_path)
     print('Done filtering out hallucinations.')
     final_output_file_path = base_name + '_filtered' + output_extension
     with open(final_output_file_path, 'w') as f:
         f.write(filtered_output)
     print(f"Filtered text written to: {final_output_file_path}")
-    print(f"Done processing {input_pdf_file_path}. See output files: Raw OCR: {raw_ocr_output_file_path} \n LLM Corrected: {output_file_path} \n LLM Corrected with Hallucinations Filtered: {final_output_file_path}\n")
+    print(f"Done processing {input_pdf_file_path}. \n\nSee output files:\n Raw OCR: {raw_ocr_output_file_path} \n LLM Corrected: {output_file_path} \n LLM Corrected with Hallucinations Filtered: {final_output_file_path}\n")
     
